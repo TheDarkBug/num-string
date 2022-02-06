bcb1485752fc2bfa3c483b55ea4a7bf99e2e49bc
TheDarkBug <adrianoliviero23@gmail.com>
2022-02-06 22:58:17 +0100

    Fixed wrong result comparison caused by new sscanf version

--- src/main.rs
+++ src/main.rs
@@ -28,17 +28,17 @@ impl Ui {
             // search for the out_range_err value
             let ore_parsed = sscanf::scanf!(l, "out_range_err = \"{}\"", String);
             // if it was found, put it in ore
-            if ore_parsed != None {
+            if !ore_parsed.is_err() {
                 ore = ore_parsed.unwrap();
             }
             // same as before, but with nan_err
             let ne_parsed = &sscanf::scanf!(l, "nan_err {} = \"{}\"", usize, String);
-            if *ne_parsed != None {
+            if !(*ne_parsed).is_err() {
                 ne[ne_parsed.as_ref().unwrap().0] = ne_parsed.as_ref().unwrap().1.clone();
             }
             // same as before, but with insert_num
             let ins_parsed = sscanf::scanf!(l, "insert_num = \"{}\"", String);
-            if ins_parsed != None {
+            if !ins_parsed.is_err() {
                 ins = ins_parsed.unwrap();
             }
         }
@@ -61,17 +61,17 @@ impl Digits {
             // search for the ones value
             let ones_parsed = sscanf::scanf!(l, "ones {} = \"{}\"", usize, String);
             // it it was found, put it in the o array at the right position
-            if ones_parsed != None {
+            if !ones_parsed.is_err() {
                 o[ones_parsed.as_ref().unwrap().0] = ones_parsed.as_ref().unwrap().1.clone();
             }
             // same as before, but for the tens
             let tens_parsed = sscanf::scanf!(l, "tens {} = \"{}\"", usize, String);
-            if tens_parsed != None {
+            if !tens_parsed.is_err() {
                 t[tens_parsed.as_ref().unwrap().0] = tens_parsed.as_ref().unwrap().1.clone();
             }
             // same as before, but for the tens
             let hundreds_parsed = sscanf::scanf!(l, "hundreds {} = \"{}\"", usize, String);
-            if hundreds_parsed != None {
+            if !hundreds_parsed.is_err() {
                 h[hundreds_parsed.as_ref().unwrap().0] =
                     hundreds_parsed.as_ref().unwrap().1.clone();
             }
@@ -87,12 +87,9 @@ impl Digits {
 
 // converts a string to a u128
 fn str_u128(src: &String, err_msg: &[String; 2]) -> u128 {
-    return src.parse::<u128>().expect(&format!(
-        "{}: \"{}\" {}",
-        err_msg[0],
-        src.chars().rev().collect::<String>(),
-        err_msg[1]
-    ));
+    return src
+        .parse::<u128>()
+        .expect(&format!("{}: \"{}\" {}", err_msg[0], src, err_msg[1]));
 }
 
 // splits the string every 3 characters from right to left
