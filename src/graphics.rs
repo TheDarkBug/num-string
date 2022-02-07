972aff9518bd65451abf821289b1881bb6f5d37e
TheDarkBug <adrianoliviero23@gmail.com>
2022-02-07 10:51:43 +0100

    Added conditional compilation for android

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,10 +1,14 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-extern crate clipboard;
 use crate::conversion;
 use crate::interface;
+#[cfg(not(target_os = "android"))]
+extern crate clipboard;
+#[cfg(not(target_os = "android"))]
 use clipboard::ClipboardContext;
+#[cfg(not(target_os = "android"))]
 use clipboard::ClipboardProvider;
+#[cfg(not(target_os = "android"))]
 use eframe::{egui, epi};
 
 struct Gui {
@@ -13,6 +17,7 @@ struct Gui {
 	input: String,
 	output: String,
 	checked: bool,
+	#[cfg(not(target_os = "android"))]
 	clipb: ClipboardContext,
 }
 
@@ -24,11 +29,13 @@ impl Gui {
 			input: String::new(),
 			output: String::new(),
 			checked: false,
+			#[cfg(not(target_os = "android"))]
 			clipb: ClipboardProvider::new().unwrap(),
 		}
 	}
 }
 
+#[cfg(not(target_os = "android"))]
 impl epi::App for Gui {
 	fn name(&self) -> &str {
 		"Num String"
@@ -46,6 +53,7 @@ impl epi::App for Gui {
 			}
 			if ui.button("Clear").clicked() {
 				self.input = String::new();
+				self.output = String::new();
 			}
 			egui::ScrollArea::vertical().show(ui, |ui| {
 				if ui.selectable_label(self.checked, &self.output).clicked() {
@@ -56,6 +64,7 @@ impl epi::App for Gui {
 	}
 }
 
+#[cfg(not(target_os = "android"))]
 pub fn run(ui: interface::Ui, digits: interface::Digits) {
 	eframe::run_native(
 		Box::new(Gui::new(ui, digits)),
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod conversion;
 use conversion::*;
 mod interface;
 use interface::*;
+#[cfg(not(target_os = "android"))]
 mod graphics;
-use graphics::*;
 
 fn usage(program: &str, opts: Options) {
     print!("{}", opts.usage(&format!("Usage: {} [options]", program)));
@@ -28,8 +28,20 @@ fn main() {
         usage(&argv[0], opts);
         return;
     }
-    // run tui version only if gui disabled
+
+    let graphics_enabled: bool;
+    #[cfg(target_os = "android")]
+    {
+        // disable graphics on android
+        graphics_enabled = false;
+    }
     if matches.opt_present("no-graphics") {
+        graphics_enabled = false;
+    } else {
+        graphics_enabled = true;
+    }
+    // run tui version only if gui disabled
+    if !graphics_enabled {
         // get language file name option (or default)
         let lang: String;
         let lang_file_name: String;
@@ -76,50 +88,28 @@ fn main() {
             println!("{}", convert(&digits, &ui, separate_nums(&user_input, &ui)));
         }
     } else {
-        // get language file name option (or default)
-        let lang: String;
-        let lang_file_name: String;
-        if matches.opt_present("lang") {
-            match matches.opt_str("l") {
-                Some(x) => lang_file_name = String::from(x.as_str()),
-                None => {
-                    eprintln!("Error: language file was not provided!");
-                    std::process::exit(1); // exit with error
+        #[cfg(not(target_os = "android"))]
+        {
+            // get language file name option (or default)
+            let lang: String;
+            let lang_file_name: String;
+            if matches.opt_present("lang") {
+                match matches.opt_str("l") {
+                    Some(x) => lang_file_name = String::from(x.as_str()),
+                    None => {
+                        eprintln!("Error: language file was not provided!");
+                        std::process::exit(1); // exit with error
+                    }
                 }
+            } else {
+                // default language file name
+                lang_file_name = String::from("langs/english.txt");
             }
-        } else {
-            // default language file name
-            lang_file_name = String::from("langs/english.txt");
+            lang = read_lang_file(lang_file_name);
+            // load the language
+            let ui = Ui::new(&lang);
+            let digits = Digits::new(&lang);
+            graphics::run(ui.clone(), digits.clone());
         }
-        lang = read_lang_file(lang_file_name);
-        // load the language
-        let ui = Ui::new(&lang);
-        let digits = Digits::new(&lang);
-        // get user input
-        // print!("{} ", ui.insert_num);
-        // stdout().flush().expect("");
-        graphics::run(ui.clone(), digits.clone());
-        // stdin().read_line(&mut user_input).expect("");
-        // // remove the '\n' from the input
-        // user_input.remove(user_input.len() - 1);
-        // if matches.opt_present("out") {
-        //     match matches.opt_str("o") {
-        //         Some(x) => {
-        //             if !(x == "stdout" || x == "/dev/stdout") {
-        //                 writeln!(
-        //                     File::create(&x).expect(&format!("Failed to open {}: {0}", &x)),
-        //                     "{}",
-        //                     convert(&digits, &ui, separate_nums(&user_input, &ui))
-        //                 )
-        //                 .expect(&format!("Failed to write to {}!", &x));
-        //                 return;
-        //             }
-        //         }
-
-        //         None => {}
-        //     }
-        // } else {
-        //     println!("{}", convert(&digits, &ui, separate_nums(&user_input, &ui)));
-        // }
     }
 }
