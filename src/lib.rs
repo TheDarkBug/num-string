0bb50cb607f90c37e79d522c66a5acb5aa22b9ad
TheDarkBug <adrianoliviero23@gmail.com>
2022-04-30 01:00:49 +0200

    Removed useless comment

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,6 @@ pub mod android {
         let lang = CStr::from_ptr(env.get_string(j_lang).unwrap().get_raw()).to_string_lossy().into_owned();
         let input = CStr::from_ptr(env.get_string(j_input).unwrap().get_raw()).to_string_lossy().into_owned();
         let digits = Digits::new(&lang);
-        // let output = format!("Out: {}\n\n\n{}", lang, input);//&convert(&digits, separate_nums(input));
         let output = &convert(&digits, separate_nums(&input));
         env.new_string(&output)
             .expect("Couldn't create java string!")
