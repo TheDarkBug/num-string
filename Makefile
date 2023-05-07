a2553c68ae1fca385dc38244f89d4a8b7abff7c3
TheDarkBug <adrianoliviero23@gmail.com>
2023-05-07 20:45:11 +0200

    Updated lib to use flutter as a frontend

--- Makefile
+++ Makefile
@@ -1,20 +1,24 @@
-ANDROID_NDK_HOME = $(HOME)/Android/Sdk/ndk
-GENERATED_V8A = target/aarch64-linux-android/release/libnum_string.so
-GENERATED_V7A = target/armv7-linux-androideabi/release/libnum_string.so
+ANDROID_LIBS_DIR = ../android/app/src/main/jniLibs
 .PHONY: langs
 
-all: dirs arm64-v8a armv7-v7a langs
+all: dirs arm64-v8a armv7-v7a current_platform load langs
 
 dirs:
-	mkdir -p ../jniLibs/{arm64-v8a, armeabi-v7a}
+	mkdir -p $(ANDROID_LIBS_DIR)/{arm64-v8a,armeabi-v7a} ../assets/langs
 
 arm64-v8a:
-	ANDROID_NDK_HOME=$(ANDROID_NDK_HOME) cargo +nightly ndk -t arm64-v8a build -Zbuild-std --release
-	cp $(GENERATED_V8A) ../jniLibs/arm64-v8a
+	ANDROID_NDK_HOME=${ANDROID_HOME}/ndk cargo +nightly ndk -t arm64-v8a build -Zbuild-std --release
 
 armv7-v7a:
-	ANDROID_NDK_HOME=$(ANDROID_NDK_HOME) cargo +nightly ndk -t armeabi-v7a build -Zbuild-std --release
-	cp $(GENERATED_V7A) ../jniLibs/armeabi-v7a
+	ANDROID_NDK_HOME=${ANDROID_HOME}/ndk cargo +nightly ndk -t armeabi-v7a build -Zbuild-std --release
+
+current_platform:
+	cargo build --lib --release
+
+load:
+	cp target/aarch64-linux-android/release/libnum_string.so $(ANDROID_LIBS_DIR)/arm64-v8a
+	cp target/armv7-linux-androideabi/release/libnum_string.so $(ANDROID_LIBS_DIR)/armeabi-v7a
+	cp target/release/libnum_string.so ../lib
 
 langs:
 	cp src/langs/english.txt ../assets/langs
--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,25 @@
-#[cfg(target_os="android")]
 mod conversion;
-#[cfg(target_os="android")]
 mod interface;
 
-#[cfg(target_os="android")]
 #[allow(non_snake_case)]
 pub mod android {
-    extern crate jni;
+    use std::ffi::{c_char, CStr, CString};
 
-    use self::jni::objects::{JClass, JString};
-    use self::jni::sys::jstring;
-    use self::jni::JNIEnv;
-    use std::ffi::CStr;
-
-    use crate::conversion::*;
-    use crate::interface::*;
+    use crate::conversion;
+    use crate::interface;
 
     #[no_mangle]
-    pub unsafe extern "C" fn Java_com_tdb_numstring_MainActivity_convert(
-        env: JNIEnv,
-        _class: JClass,
-        j_input: JString,
-        j_lang: JString,
-    ) -> jstring {
-        let lang = CStr::from_ptr(env.get_string(j_lang).unwrap().get_raw()).to_string_lossy().into_owned();
-        let input = CStr::from_ptr(env.get_string(j_input).unwrap().get_raw()).to_string_lossy().into_owned();
-        let digits = Digits::new(&lang);
-        let output = &convert(&digits, separate_nums(&input));
-        env.new_string(&output)
-            .expect("Couldn't create java string!")
-            .into_inner()
+    pub unsafe extern "C" fn convert(
+        inputc_str: *const c_char,
+        langc_str: *const c_char,
+    ) -> *const c_char {
+        let input = CStr::from_ptr(inputc_str).to_str().unwrap();
+        let lang = CStr::from_ptr(langc_str).to_str().unwrap();
+        if lang.is_empty() {
+            return CString::new("").unwrap().into_raw();
+        }
+        let digits = interface::Digits::new(&lang);
+        let output = &conversion::convert(&digits, conversion::separate_nums(input));
+        return CString::new(&**output).unwrap().into_raw();
     }
 }
